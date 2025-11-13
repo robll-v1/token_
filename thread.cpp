e27ccf93ede69a771394f8b7fccdac43775d3ba7
robll-v1 <3922295484@qq.com>
2025-11-13 18:19:23 +0800

    fix

--- thread.cpp
+++ thread.cpp
@@ -4,9 +4,44 @@
 #include <condition_variable>
 #include <mutex>
 
-int main () {
-    std::thread t1(producer);
-    std::thread t2(consumer);
+class Queue {
+public:
+    void put (int val) {
+        std::unique_lock<std::mutex> lock(mtx);
+        q.push(val);
+        std::cout << "producer: " << val << std::endl;
+    }
 
-    t1.join
+    int get () {
+        std::unique_lock<std::mutex> lock(mtx);
+        int val = q.front();
+        q.pop();
+        std::cout << "consumer: " << val << std::endl; 
+        return val;
+    }
+private:
+    std::queue <int> q;
+    std::mutex mtx;
+};
+void producer (Queue *q) {
+    for (int i = 1; i <= 100; i++) {
+        q->put(i);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void consumer (Queue *q) {
+    for (int i = 1; i <= 100; i++) {
+        q->get();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+int main () {
+    Queue q;
+    std::thread t1(producer, &q);
+    std::thread t2(consumer, &q);
+    
+    t1.join();
+    t2.join();
+
 }
